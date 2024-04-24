14f6edd30ed4bfce31cf6c3dc6abebd8650e98c7
ashkar@21 <ashkarrahim21@gmail.com>
2024-04-24 17:30:28 +0530

    add page full correct no morechange in this

--- OnGoing/manga_admin/lib/widgets/drop_down_button2/multidrop_down_button2.dart
+++ OnGoing/manga_admin/lib/widgets/drop_down_button2/multidrop_down_button2.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:multiselect_dropdown_pro/multiselect_dropdown_pro.dart';
 
 class MulitDrop2 extends StatefulWidget {
   final TextEditingController controller;
@@ -22,95 +22,22 @@ class _MulitDrop2State extends State<MulitDrop2> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: DropdownButton2<String>(
-        isExpanded: true,
-        hint: Text(
-          widget.hintText,
-          style: TextStyle(
-            fontSize: 14,
-            color: Theme.of(context).hintColor,
-          ),
-        ),
-        
-        items: widget.items.map((items) {
-          return DropdownMenuItem<String>(
-            value: items,
-            //disable default onTap to avoid closing menu when selecting an item
-            onTap: () {},
-            child: StatefulBuilder(
-              builder: (context, menuSetState) {
-                final isSelected = selectedItems.contains(items);
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      isSelected
-                          ? selectedItems.remove(items)
-                          : selectedItems.add(items);
-                    });
-                    //This rebuilds the dropdownMenu Widget to update the check mark
-                    menuSetState(() {});
-                  },
-                  child: Container(
-                    height: 40,
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Row(
-                      children: [
-                        if (isSelected)
-                          const Icon(Icons.check_box_outlined)
-                        else
-                          const Icon(Icons.check_box_outline_blank),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Text(
-                            items,
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          );
-        }).toList(),
-        //Use last selected item as the current value so if we've limited menu height, it scroll to last item.
-        value: selectedItems.isEmpty ? null : selectedItems.last,
-        onChanged: (String? value) {
+    return MultiSelectDropDown(
+      title: 'Select Options',
+      items: widget.items,
+      onChanged: (String? selectedItems) {
+        print('Selected Items: $selectedItems');
+        if (selectedItems != null) {
           setState(() {
-            print(selectedItems);
-            selectedItems = value?.split(', ') ?? [];
-            widget.controller.text = selectedItems.join(', ');
+            widget.controller.text = selectedItems;
           });
-        },
-        selectedItemBuilder: (context) {
-          return [
-            Container(
-              alignment: AlignmentDirectional.center,
-              child: Text(
-                selectedItems.join(', '),
-                style: const TextStyle(
-                  fontSize: 14,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                maxLines: 1,
-              ),
-            ),
-          ];
-        },
-        buttonStyleData: const ButtonStyleData(
-          padding: EdgeInsets.only(left: 16, right: 8),
-          height: 40,
-          width: 140,
-        ),
-        menuItemStyleData: const MenuItemStyleData(
-          height: 40,
-          padding: EdgeInsets.zero,
-        ),
-      ),
+        }
+      },
+      value: '', // Initial selected values
+      hint: 'Select options',
+      primaryColor: Colors.white, // Customize primary color
+      suffixIcon: Icons.arrow_drop_down, // Customize suffix icon
+      separator: ',', // Customize separator
     );
   }
 }
